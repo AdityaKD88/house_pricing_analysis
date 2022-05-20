3361c6f813df65935b129f1e55c6f57c6f683a69
AdityaKD88 <90971914+AdityaKD88@users.noreply.github.com>
2022-05-20 18:00:47 +0530

    prediction

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ import pandas as pd
 import json
 import plotly
 import plotly.express as px
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from joblib import load
 app = Flask(__name__)
 
 df = pd.read_csv('Mumbai1.csv')
+
+model = load('model.pkl')
 
 @app.route('/')
 def index():
@@ -106,6 +109,32 @@ def price():
 
     return render_template('price.html',graph15=graph15,graph16=graph16,graph17=graph17)
 
+@app.route('/predict', methods=['GET','POST'])
+def predict():
+    if request.method=="POST":
+        form = request.form
+        area = int(form.get('area'))
+        bed = int(form.get('bed'))
+        new = int(form.get('new'))
+        gym = int(form.get('gym'))
+        lift = int(form.get('lift'))
+        car = int(form.get('car'))
+        maintain = int(form.get('maintain'))
+        sec = int(form.get('sec'))
+        club = int(form.get('club'))
+        intercom = int(form.get('intercom'))
+        garden = int(form.get('garden'))
+        games = form.get('games')
+        gas = int(form.get('gas'))
+        jog = float(form.get('jog'))
+        swim = int(form.get('swim'))
+        userinp = [[area,bed,new,gym,lift,car,maintain,sec,club,intercom,garden,games,gas,jog,swim]]
+        prediction = model.predict(userinp)
+        output = round(prediction[0], 2)
+        return render_template('predict.html',area=area,bed=bed,new=new,gym=gym,lift=lift,car=car,
+                                maintain=maintain,sec=sec,club=club,intercom=intercom,
+                                garden=garden,games=games,gas=gas,jog=jog,swim=swim,output=output)
+    return render_template('predict.html')
 if __name__ == '__main__':
   app.run(host='127.0.0.1', port=8000, debug=True)
  
